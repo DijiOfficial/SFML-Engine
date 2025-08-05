d44d9cabc12c7d863bd5584978e5eac5547427f1
Dylan Burgisser <33450259+DijiOfficial@users.noreply.github.com>
2025-08-05 15:16:37 +0200

    feat: added base setup for zombie arena game chapter 8 start

---
+++ Zombie Arena/Core/GameLoader.cpp
@@ -0,0 +1,42 @@
+﻿#include "GameLoader.h"
+#include "GameState.h"
+#include "Engine/Singleton/SceneManager.h"
+#include "Engine/Input/InputManager.h"
+#include "Engine/Interfaces/ISoundSystem.h"
+
+using namespace diji;
+
+namespace global // todo: remove this and use the singleton with the window
+{
+    constexpr static sf::Vector2u VIEWPORT{ 1920, 1080 };
+}
+
+void SceneLoader::ZombieArena() 
+{
+#ifndef NDEBUG
+    ServiceLocator::RegisterSoundSystem(std::make_unique<LoggingSoundSystem>(std::make_unique<SFMLISoundSystem>()));
+#else
+    ServiceLocator::RegisterSoundSystem(std::make_unique<SFMLISoundSystem>());
+#endif
+    
+    const auto& scene = SceneManager::GetInstance().CreateScene(static_cast<int>(zombieArena::GameState::Level));
+    SceneManager::GetInstance().SetActiveScene(static_cast<int>(zombieArena::GameState::Level));
+
+
+#pragma region Commands
+    // auto& input = InputManager::GetInstance();
+    //
+    // input.BindCommand<pong::MovePlayer>(PlayerIdx::KEYBOARD, KeyState::PRESSED, sf::Keyboard::Scancode::Left, paddle, pong::Movement::Left, true);
+    // input.BindCommand<pong::MovePlayer>(PlayerIdx::KEYBOARD, KeyState::RELEASED, sf::Keyboard::Scancode::Left, paddle, pong::Movement::Left, false);
+    // input.BindCommand<pong::MovePlayer>(PlayerIdx::KEYBOARD, KeyState::PRESSED, sf::Keyboard::Scancode::Right, paddle, pong::Movement::Right, true);
+    // input.BindCommand<pong::MovePlayer>(PlayerIdx::KEYBOARD, KeyState::RELEASED, sf::Keyboard::Scancode::Right, paddle, pong::Movement::Right, false);
+#pragma endregion
+
+#pragma region Observers
+    // ball->GetComponent<pong::Ball>()->OnLifeLostEvent.AddListener(livesCounter->GetComponent<ScoreCounter>(), &ScoreCounter::DecreaseScore);
+    // ball->GetComponent<pong::Ball>()->OnIncreaseScoreEvent.AddListener(scoreCounter->GetComponent<ScoreCounter>(), &ScoreCounter::IncreaseScore);
+    //
+    // livesCounter->GetComponent<ScoreCounter>()->OnGivenScoreReachedEvent.AddListener(livesCounter->GetComponent<ScoreCounter>(), &ScoreCounter::Reset);
+    // livesCounter->GetComponent<ScoreCounter>()->OnGivenScoreReachedEvent.AddListener(scoreCounter->GetComponent<ScoreCounter>(), &ScoreCounter::Reset);
+#pragma endregion
+}
---
+++ Zombie Arena/Core/GameState.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+namespace zombieArena 
+{
+    enum class GameState
+    {
+        Invalid = -1,
+        Level,
+        GameOver,
+    };
+}
