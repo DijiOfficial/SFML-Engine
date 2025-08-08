9b0ac486755444abd16908d4b0ee1566b7f97a16
Dylan Burgisser <33450259+DijiOfficial@users.noreply.github.com>
2025-08-08 11:29:04 +0200

    perf: made the creation of random devices static and unique

--- Engine/Engine/Singleton/RandNumber.h
+++ Engine/Engine/Singleton/RandNumber.h
@@ -3,19 +3,19 @@
 
 namespace diji
 {
-    class RandNumber
+    static thread_local std::mt19937 rng(std::random_device{}()); // cool thing I did not know, generating a device is slow and so only doing it once is good optimization
+
+    class RandNumber final
     {
     public:
         static int GetRandomRangeInt(const int min, const int max)
         {
-            static thread_local std::mt19937 rng(std::random_device{}());
             std::uniform_int_distribution<int> dist(min, max);
             return dist(rng);
         }
 
         static float GetRandomRangeFloat(const float min, const float max)
         {
-            static thread_local std::mt19937 rng(std::random_device{}());
             std::uniform_real_distribution<float> dist(min, max);
             return dist(rng);
         }
