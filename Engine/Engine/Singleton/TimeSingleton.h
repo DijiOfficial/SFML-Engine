a7fe0be057d1dcaeeff3a7ab0956e508a258a456
Dylan Burgisser <dylan.burgisser@student.howest.be>
2025-09-17 11:29:12 +0100

    feat: added fixedTimeStep to TimeSingleton

--- Engine/Engine/Singleton/TimeSingleton.h
+++ Engine/Engine/Singleton/TimeSingleton.h
@@ -6,10 +6,14 @@ namespace diji
     class TimeSingleton final : public Singleton<TimeSingleton>
     {
     public:
-        float GetDeltaTime() const { return m_DeltaTime; }
+        [[nodiscard]] float GetDeltaTime() const { return m_DeltaTime; }
         void SetDeltaTime(const float deltaTime) { m_DeltaTime = deltaTime; }
+
+        [[nodiscard]] float GetFixedUpdateDeltaTime() const { return m_FixedTimeStep; }
+        void SetFixedUpdateDeltaTime(const float fixedTimeStep) { m_FixedTimeStep = fixedTimeStep; }
 
     private:
         float m_DeltaTime = 0.0f;
+        float m_FixedTimeStep = .0f;
     };
 }
